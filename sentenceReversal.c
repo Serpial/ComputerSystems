38605fa8b5ff8b2c05be543dab186abbf8427aa7
serpial <phutchison8@gmail.com>
2018-10-11 21:42:25 +0100

    started the last question

--- sentenceReversal.c
+++ sentenceReversal.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#define BASE_SENTENCE_SIZE 250
+
+/* Function Prototypes */
+/* void getInput(char *sentence[BASE_SENTENCE_SIZE]); */
+
 
 int main(void) {
+  char sentence[BASE_SENTENCE_SIZE];
+
+  /* getInput(&sentence); */
+  printf("Please enter a sentence that ends in a period, question mark, or exclamation point\n");
+  printf("Enter sentence here : ");
+
+  fgets(sentence, sizeof(sentence), stdin);
+
+
+
+  
+}
+
+void getInput(char *sentence[BASE_SENTENCE_SIZE]) {
   
 }
