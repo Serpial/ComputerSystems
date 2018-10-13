4a2f04d0c892ba5ae22795ef0af69bbee4c03ebe
serpial <serpial@github.com>
2018-10-14 00:45:12 +0100

    fixed odd bug and added functions

--- sentenceReversal.c
+++ sentenceReversal.c
@@ -15,55 +15,72 @@
 #define MAX_WORDS 10 /* maximum number of words the user can have in their sentence */
 
 /* Function Prototypes */
-/* none */
+int findIndexOfBreak (char sentence[CHAR_SENTENCE_SIZE]);
+
+void getWordArray (char sentence[CHAR_SENTENCE_SIZE], int spaceOfBreak, int *numWords,)
 
 int main(void) {
   char sentence[CHAR_SENTENCE_SIZE];
   char words [MAX_WORDS] [CHAR_NUM_WORDS];
   char terminatingChar= '.';
-  int goodChar = 0, numWords = 0, spaceOfBreak = -1;
+  int goodChar = 0, numWords = 0, spaceOfBreak;
   int i,j;
-
-  /* getInput(&sentence); */
+  
   printf("Please enter a sentence that ends in a period, question mark, or exclamation point\n");
   printf("Enter sentence here : ");
-
   fgets(sentence, sizeof(sentence), stdin);
+  
+  spaceOfBreak = findIndexOfBreak(sentence);
+  if (spaceOfBreak == 0) {
+    return 0;
+  }
+  
+  terminatingChar = sentence[spaceOfBreak];
 
+  /* form a collection of words from the sentence */
+  for (i = 0; i < spaceOfBreak; i++) {
+    if (sentence[i] == ' ') {
+      words[numWords++][goodChar] = '\0';
+      /* On to the next word */
+      if (sentence[i+1] != ' ' && spaceOfBreak != i+1) {
+        words[numWords][0] = sentence[++i];
+        goodChar = 1;
+      }
+    } else {
+      words[numWords][goodChar++] = sentence[i];
+    }
+  }
+  words[numWords][goodChar] = '\0';
+
+  printf("Your sentence backwards is : ");
+  
+  /* print words backwards */
+  for (i = numWords; i >= 0; i--) {
+    printf("%s ", words[i]);
+  }
+  printf("%c%c\n",'\b',terminatingChar);
+    
+ 
+  return 0;
+}
+
+int findIndexOfBreak (char sentence[CHAR_SENTENCE_SIZE]) {
+  int i; /* Index */
+  int found = 0; /* bool found = false; */
+  
   for (i = 0; i < CHAR_SENTENCE_SIZE; i++) {
     switch (sentence[i]) {
     case '!': case '?': case '.':
-      terminatingChar = sentence[i];
-      spaceOfBreak = i;
+      found++; /* sets found to true if found */
     }
+    if (found) break;
   }
 
-  /* form a collection of words from the sentence */
-  if (spaceOfBreak != -1){
-    for (i = 0; i < spaceOfBreak; i++) {
-      if (sentence[i] == ' ') {
-        words[numWords++][goodChar] = '\0';
-        /* On to the next word */
-        if (sentence[i+1] != ' ' && spaceOfBreak != i+1) {
-          words[numWords][0] = sentence[++i];
-          goodChar = 1;
-        }
-      } else {
-        words[numWords][goodChar++] = sentence[i];
-      }
-    }
-    words[numWords][goodChar] = '\0';
-
-    printf("Your sentence backwards is : \n");
-    
-    /* print words backwards */
-    for (i = numWords; i >= 0; i--) {
-      printf("%s ", words[i]);
-    }
-    printf("%c%c\n",'\b' ,terminatingChar);
+  if (!found || i == 168) {
+    printf("Please make sure to finish you sentence with a '.', '!', or '?'");
+    return 0;
   } else {
-    printf("Please make sure you end your sentence with either a !, ?, or .\n");
+    return i;
   }
- 
 }
 
