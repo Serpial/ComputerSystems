d190a410f0a82543764cceb31f8884d489771c16
serpial <serpial@github.com>
2018-11-15 00:31:32 +0000

    added displayMemBin - displaying memory in binary

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-#include "fileInput.h"
+#include "twoCompConv.h"
 
 #define INST_SIZE 16 // Instruction size
+#define BITS_NUM_WORDS 12+1 // 2^12=4096 plus 1 for twos comp
+
+void displayMemBin();
+void getMemFromFile(char *fileLocation);
 
 int16_t memory[4096];
 // Registers
@@ -14,7 +18,40 @@ uint16_t *AC;
 uint16_t *MAR; // has to be 12 bits
 
 int main(/* to fill later */) {
-  getMemFromFile("./binTest", memory, INST_SIZE );
+  getMemFromFile("./binTest");
+  displayMemBin();
   
   return 0;
+}
+
+void displayMemBin () {
+	int counter=0;
+	printf("Memory Location\t->\tInstruction\n"); // Heading
+	char memLoc[BITS_NUM_WORDS+1], instruc[INST_SIZE+1];
+
+	while(memory[counter]!=0) {
+		strcpy(memLoc, decToBinStr(counter,BITS_NUM_WORDS));
+		strcpy(instruc, decToBinStr(memory[counter], INST_SIZE));
+		printf("%s\t->\t%s\n", memLoc, instruc);
+		counter++;
+	}
+}
+
+void getMemFromFile(char *fileLocation) {
+
+  FILE *fp;
+  int counter=0;
+  char buffer[0xFF];
+
+  fp=fopen(fileLocation, "r");
+
+  if (fp != NULL) {
+    while(fscanf(fp,"%20s",buffer) == 1) {
+        memory[counter]=binToDec(buffer);
+        counter++;
+    }
+  } else {
+    printf("This file does not exist\n");
+  }
+  fclose(fp);
 }
